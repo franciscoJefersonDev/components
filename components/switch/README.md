7cc12bdca1d229a53682258239c2ff391db1e712
franciscoJefersonDev <francisco.jeferson.dev@gmail.com>
2022-07-10 17:43:29 +0000

    changes

---
+++ components/switch/README.md
@@ -0,0 +1,3 @@
+#SWITCH
+##DEMO
+### https://stackblitz.com/github/franciscoJeferson/components_ui/tree/main/components/switch/dist
--- components/switch/src/main.ts
+++ components/switch/src/main.ts
@@ -4,7 +4,6 @@ const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
   <label class="switch">
-    Toggle
     <input type="checkbox" class="switch__input">
     <span class="switch__slider"></span>
   </label>

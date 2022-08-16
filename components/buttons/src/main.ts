6ef7c528e68871b26fd0025a10915466bdb9a9d8
franciscoJefersonDev <francisco.jeferson.dev@gmail.com>
2022-08-16 12:31:14 +0000

    changes-in-buttons

--- components/buttons/src/main.ts
+++ components/buttons/src/main.ts
@@ -2,11 +2,9 @@ import "./styles.scss";
 import "./buttons.scss";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 const component = `
-  <h5>.wk-btn</h5>
   <button class="wk-btn">
-  Default
+    Default
   </button>
-  <h5>.wk-btn .navbar</h5>
   <button class="wk-btn navbar">
     <ion-icon name="more"></ion-icon>
   </button>
